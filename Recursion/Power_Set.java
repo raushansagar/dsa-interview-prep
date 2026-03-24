0400de97adac15c83f35cb99415afd05db1d249f
RaushanSagar1694 <dayasagar1694@gmail.com>
2026-03-24 17:48:19 +0530

    power_set

--- Recursion/Power_Set.java
+++ Recursion/Power_Set.java
@@ -1,8 +1,36 @@
-package Interview_DS_Algo.Recursion;
+/*
+    Power Set
+    Leetcode : https://www.geeksforgeeks.org/problems/power-set4302/1
+ */
 
 
-class sag{
-    public static void main(String[] args) {
-        System.out.println("Sfdsfsaf");
+
+import  java.util.*;
+class solution {
+    public void solve(int idx, String s, int n, String curr, List<String> result){
+        if(idx >= n){
+           if(curr.length() > 0) result.add(curr);
+            return;
+        }
+        
+        
+        // take 
+        curr = curr + s.charAt(idx);
+        solve(idx+1, s, n, curr, result);
+        
+        // backtrack
+        curr = curr.substring(0, curr.length()-1);
+        
+        // not take
+        solve(idx+1, s, n, curr, result);
+    }
+    public List<String> AllPossibleStrings(String s) {
+        int n = s.length();
+        List<String> result = new ArrayList<>();
+        
+        solve(0, s, n, "", result);
+        Collections.sort(result);
+        
+        return result;
     }
 }
